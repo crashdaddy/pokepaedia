fc89b1fdb7f3afdc91940c8d65c49e4b832d250f
crashdaddy <kennethscoggins@mail.com>
2020-12-03 17:42:03 -0600

    added component for each move

--- src/Components/TypeMoves/TypeMoves.js
+++ src/Components/TypeMoves/TypeMoves.js
@@ -6,7 +6,9 @@ class TypeMoves extends Component {
     
     this.state = {
         typeMoves: [],
-        pokemonTypeID: this.props.pokemonTypeID || 1
+        pokemonTypeID: this.props.pokemonTypeID || 1,
+        moveData: [],
+        flavorText: ''
      }
    }
 
@@ -25,26 +27,38 @@ class TypeMoves extends Component {
     
 //   }
 
-//   getData = () => {
-//     let url = "https://pokeapi.co/api/v2/type/" + this.props.pokemonTypeID;
-//     fetch(url)
-//     .then(res=>res.json())
-//     .then(data => {
-//       this.setState({
-//         typeMoves: data.moves
-//       })
-//     })
-//   }
+  changeMove = (moveUrl) => {
+    
+    fetch(moveUrl)
+    .then(res=>res.json())
+    .then(data => {
+      let flavorText= '';
+      if(data.flavor_text_entries[2]) {
+          flavorText=data.flavor_text_entries[2].flavor_text
+      }
+      this.setState({
+        moveData: data.effect_entries,
+        flavorText: flavorText
+      })
+    })
+  }
 
   render() {
     return (
-        <div>
+        <div style={{marginTop:'20px',color:'red',fontWeight:'bold'}}>
        {this.props.typeMoves && this.props.typeMoves.map(pokemonMovesData  => {
    
-         return( <span style={{marginLeft:'5px'}}>
+         return( <span style={{marginLeft:'5px',color:'black'}} onClick={()=>this.changeMove(pokemonMovesData.url)}>
              {pokemonMovesData.name} 
         </span>)   
-      })}     
+      })}
+      <p/>
+      {this.state.moveData && this.state.moveData.map(moveDetails => {
+          return(
+              <span style={{marginLeft:'5px'}} >{moveDetails.short_effect}</span>
+          )
+      } )}  
+        <div style={{marginTop:'20px'}}>{this.state.flavorText}</div>   
         </div>
   
     );
