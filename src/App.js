b7387971764908b5f5d8e68aefb098c32fc4e9c8
crashdaddy <kennethscoggins@mail.com>
2020-12-03 15:59:30 -0600

    added filter for type

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {React, Component} from 'react';
+import MainBoard from './Components/MainBoard/MainBoard'
 import './App.css';
 
 class App extends Component {
@@ -7,7 +8,8 @@ class App extends Component {
     
     this.state = {
          types: [],
-         page: 1
+         page: 1,
+         pokemonType: 1,
      }
    }
 
@@ -21,20 +23,35 @@ class App extends Component {
     fetch(url)
     .then(res=>res.json())
     .then(data => {
-      console.log(data.results)
       this.setState({
         types: [...this.state.types,...data.results]
       })
     })
   }
 
+  changePokemonType = (newType) => {
+    console.log(newType);
+    this.setState({
+      pokemonType: newType
+    })
+  }
+
   render() {
     return (
       <div>
-       {this.state.types.map(typeData  => {return <div>
-         <button href={typeData.url} style={{marginBottom:'5px'}} >{typeData.name}</button>
-        </div>   
+        <div style={{float:'left',width:'10%'}}>
+        <img src="banner.png" alt="banner" style={{width:'100%'}} />
+       {this.state.types.map(typeData  => {
+         let idStr = typeData.url.split('/');
+         let pokemonTypeID = idStr[6];
+         return( <div>
+         <button href={typeData.url} style={{marginBottom:'5px',width:'90%'}}  onClick={() => this.changePokemonType(pokemonTypeID)}>{typeData.name}</button>
+        </div>)   
       })}     
+        </div>
+        <div style={{width:'50%',border:'1px solid black',float:'left'}}> 
+          <MainBoard pokemonTypeID={this.state.pokemonType} />
+        </div>
       </div>
     );
   }
---
+++ src/Components/MainBoard/MainBoard.js
@@ -0,0 +1,62 @@
+import {React, Component} from 'react';
+
+class MainBoard extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+        pokemon: [],
+        pokemonTypeID: 1
+     }
+   }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  componentDidUpdate() {
+
+    if(this.props.pokemonTypeID !== this.state.pokemonTypeID) {
+      this.setState({
+        pokemonTypeID: this.props.pokemonTypeID
+      })
+      this.getData();
+    }
+    
+  }
+
+  getData = () => {
+    let url = "https://pokeapi.co/api/v2/type/" + this.props.pokemonTypeID;
+    console.log(url)
+    fetch(url)
+    .then(res=>res.json())
+    .then(data => {
+      console.log(data.pokemon)
+      this.setState({
+        pokemon: data.pokemon
+      })
+    })
+  }
+
+  addDefaultSrc(ev){
+    ev.target.src = 'noImg.png'
+  }
+
+  render() {
+    return (
+        <div>
+       {this.state.pokemon && this.state.pokemon.map(pokemonData  => {
+         let idStr = pokemonData.pokemon.url.split('/');
+         let pokemonID = idStr[6];
+         let imageURL = "https://pokeres.bastionbot.org/images/pokemon/"+ pokemonID + ".png";
+         return( <div>
+        <img src={imageURL} style={{width:'50px',float:'left',margin:'5px'}}  onError={this.addDefaultSrc} />
+        </div>)   
+      })}     
+        </div>
+  
+    );
+  }
+}
+
+export default MainBoard;
